878ff1e5c2c9e0aea5988b94d59e9fcb55dd1ac9
saadabban76 <saadabban76@gmail.com>
2024-01-22 00:16:36 +0530

    worked on home page

--- src/frontend/src/components/HomePage.vue
+++ src/frontend/src/components/HomePage.vue
@@ -4,15 +4,11 @@
     <div class="relative py-16">
 
       <div class="mx-auto max-w-7xl px-6 lg:px-8">
-        <h1 class="p-1 mx-auto w-max px-4 rounded-3xl text-white bg-purple-500 text-[1.2rem] mt-10">Made with ❤️
-          using <a class="hover:text-black border-black border-none "
-            href="https://github.com/dfinity/interface-spec/pull/158">
-            VetKD >
-          </a>
-        </h1>
-        <div class="mt-2 flow-root">
-          <div class="relative mt-10 mx-auto max-w-[90%] text-center flex gap-3 flex-col justify-center items-center">
-
+      t-10 mx-auto max-w-[90%] text-center flex gap-3 flex-col justify-center items-center">
+      <RouterLink v-else :to="{ name: 'admin' }"
+                class="rounded-md bg-[#2A2523] px-10 py-3 text-sm font-semibold hover:text-black shadow-sm hover:bg-highlight text-highlight border border-[#8b712f] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#2A2523]">
+                View Forms
+              </RouterLink>
             <h1
               class="font-bold mx-auto w-[100%] sm:w-[80%] tracking-tight text-gray-100 xl:w-[100%] xl:text-[5rem] text-6xl">
               Secure Forms on
@@ -34,10 +30,15 @@
                 class="rounded-md  px-3.5 py-2.5 text-sm font-semibold text-highlight border border-[#8b712f] shadow-sm hover:bg-highlight focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">
                 Get started
               </button>
-              <RouterLink v-else :to="{ name: 'admin' }"
-                class="rounded-md bg-[#2A2523] px-10 py-3 text-sm font-semibold hover:text-black shadow-sm hover:bg-highlight text-highlight border border-[#8b712f] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#2A2523]">
-                View Forms
-              </RouterLink>
+
+              <h1 class="p-1 mx-auto w-max px-4 rounded-3xl text-white bg-purple-500 text-[1.2rem] mt-10">Made with ❤️
+          using <a class="hover:text-black border-black border-none "
+            href="https://github.com/dfinity/interface-spec/pull/158">
+            VetKD >
+          </a>
+        </h1>
+        <div class="mt-2 flow-root">
+          <div class="relative m
             </div>
           </div>
         </div>
