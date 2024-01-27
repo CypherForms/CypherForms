b0309e0417eb1318b361f2e6e7525ea9d572adaa
saadabban76 <saadabban76@gmail.com>
2024-01-27 19:06:58 +0530

    updated app.vue

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -7,12 +7,12 @@ import { useHead } from '@vueuse/head'
 
 useHead({
   titleTemplate: (title?: string) =>
-    !title ? 'CypherForms | accessible forms on the IC with End-to-End Encryption' : `${title} | CypherForms`,
+    !title ? 'CypherForms | accessible forms on the IC with End-to-End Encryption' : `${title} | CypherForm s`,
   meta: [
     {
       name: 'description',
       content:
-        'Build Fast, accessible forms on the IC with End-to-End Encryption where it counts. Because protecting your data should be effortless.'
+        'Build Fast, accessible forms on the IC with End-to-End Encryption where it counts. Because protecting your data should be effortless and easy.'
     }
   ]
 })
