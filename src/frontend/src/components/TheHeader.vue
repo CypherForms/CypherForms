67ab9212532bc23e243dd6b91b352eadadeb0167
saadabban76 <saadabban76@gmail.com>
2024-01-28 10:28:54 +0530

    updated header.jsx

--- src/frontend/src/components/TheHeader.vue
+++ src/frontend/src/components/TheHeader.vue
@@ -25,6 +25,11 @@
               <component v-if="item.icon" :is="item.icon" class="h-5 w-5 flex-none" aria-hidden="true" />
               {{ item.name }}
             </RouterLink>
+            <PopoverButton
+              class="inline-flex items-center gap-x-1 px-3 py-2 text-sm font-semibold leading-6 text-white outline-none rounded-full hover:text-black hover:bg-[#ffc04a]">
+              <span>Account</span>
+              <ChevronDownIcon class="h-5 w-5" aria-hidden="true" />
+            </PopoverButton>
           </div>
         </div>
         <div class="justify-end flex">
@@ -32,11 +37,7 @@
             Log in <span aria-hidden="true">&rarr;</span>
           </button>
           <Popover v-if="authStore.actor" class="relative">
-            <PopoverButton
-              class="inline-flex items-center gap-x-1 px-3 py-2 text-sm font-semibold leading-6 text-white outline-none rounded-full hover:text-black hover:bg-[#ffc04a]">
-              <span>Account</span>
-              <ChevronDownIcon class="h-5 w-5" aria-hidden="true" />
-            </PopoverButton>
+          
 
 
             <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1"
