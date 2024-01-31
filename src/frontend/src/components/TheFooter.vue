05d4e927797b1f36dc494da0ee7e4bf42d0ba2ee
richen <jirachain538@gmail.com>
2024-01-31 10:55:49 +0530

    clearning up some stuff

--- src/frontend/src/components/TheFooter.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="w-full bg-[#0d0e11] xl:text-[1.4rem] text-[1.3rem] p-8 text-center text-xs text-gray-300">
-    <span className="text-red-300">Disclaimer :</span> Do not use this in production.
-  </div>
-</template>
--- src/frontend/vite-plugin-wasm.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'vite-plugin-wasm' {
-    const wasm: any;
-    export default wasm;
-}
